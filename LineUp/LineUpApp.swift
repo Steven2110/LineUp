0cc57720f136637825eb7ef12c72997af3abe8c4
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-02-24 03:23:25 +0700

    Initial Commit

---
+++ LineUp/LineUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineUpApp.swift
+//  LineUp
+//
+//  Created by Steven Wijaya on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct LineUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
